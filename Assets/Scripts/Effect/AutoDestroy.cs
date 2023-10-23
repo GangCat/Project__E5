c7c7ea6a513793b846e2031ad63dd25f4639da4a
GangCat <windy9300@gmail.com>
2023-10-23 23:58:02 +0900

    안개 밖으로 용암 보이던거 가림
    처음에 맵 생성할때 쓴 장애물 5초뒤에 제거하는 코드 추가

--- Assets/Scripts/Effect/AutoDestroy.cs
+++ Assets/Scripts/Effect/AutoDestroy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoDestroy : MonoBehaviour
+public class MyAutoDestroyObstacle : MonoBehaviour
 {
     private void Start()
     {
