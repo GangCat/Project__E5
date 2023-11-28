9e23cb2a15ea1e57077eecf54351aee32e904c6b
GangCat <windy9300@gmail.com>
2023-11-28 15:41:43 +0900

    ㅁㄴㅇㄹ

--- Assets/Scripts/TempButton.cs
+++ Assets/Scripts/TempButton.cs
@@ -9,6 +9,11 @@ public class TempButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Button>().onClick.AddListener(
+            () =>
+            {
+                ArrayChangeHotkeyCommand.Use(EChangeHotkeyCommand.SELECT_UNIT_FUNC_BUTTON, EUnitFuncKey.ATTACK);
+            });
     }
 
 
